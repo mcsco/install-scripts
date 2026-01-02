1d6bbc834abffe325798b52b93da2c02c8c742ec
mcsco <michael@cloudadrift.com>
2026-01-02 15:02:36 -0600

    tweaks add. source devangshekhawat repo.

--- fedora/fedora-kde-install.sh
+++ fedora/fedora-kde-install.sh
@@ -10,8 +10,9 @@ countme=False
 
 # Setup RPMFusion
 sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
-# For dnf5
+# Install app-stream metadata
 sudo dnf group upgrade core -y
+sudo dnf4 group install core -y
 # For dnf4
 # sudo dnf groupupdate core -y
 
@@ -28,12 +29,19 @@ sudo dnf remove -y anaconda* \
 	atmel-firmware libertas-usb8388-firmware abrt* open-vm-tools nano nano-default-editor sos cyrus-sasl-plain spice-vdagent adcli realmd vpnc xorg-x11-drv-vmware \
 	hyperv* virtualbox-guest-additions qemu-guest-agent kmines kpat akregator kamoso konversation kmahjongg kmouth kcharselect libreoffice-core* kwrite
 
-# Run Updates
+# Remove Orphan packages
 sudo dnf autoremove -y
-sudo fwupdmgr get-devices
+
+# Run Updates
 sudo fwupdmgr refresh --force
-sudo fwupdmgr get-updates -y
-sudo fwupdmgr update -y
+sudo fwupdmgr get-devices # Lists devices with available updates.
+sudo fwupdmgr get-updates # Fetches list of available updates.
+sudo fwupdmgr update
+
+# Enable Flathub
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+# Fedora doesn't enable Flatpak user-home installation by default
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Setup Flathub beta and third party packages
 sudo fedora-third-party enable
@@ -41,15 +49,31 @@ sudo fedora-third-party refresh
 flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
 
 # Hardware codecs with Intel
-sudo dnf install intel-media-driver
+sudo dnf swap libva-intel-media-driver intel-media-driver --allowerasing
+sudo dnf install libva-intel-driver
+# sudo dnf install intel-media-driver -- not required due to above commands
 
 # Hardware codecs with AMD (mesa)
 sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld
 sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
-
-# 32-bit compatibility (for steam or alikes)
 sudo dnf swap mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686
 sudo dnf swap mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686
+
+# NVIDIA Drivers
+mokutil --sb-state # checks to see if secure boot is enabled
+#
+sudo dnf install akmod-nvidia
+sudo dnf install xorg-x11-drv-nvidia-cuda
+# Wait 5 minutes before rebooting to let time for the kernel module to be built
+modinfo -F version nvidia # checks to see if the kernel module was built
+
+# Hardware Video Acceleraion for Firefox
+sudo dnf install -y openh264 gstreamer1-plugin-openh264 mozilla-openh264
+sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
+# After this enable the OpenH264 Plugin in Firefox's settings.
+
+# Remove Default Fedora Firefox Start Page
+sudo rm -f /usr/lib64/firefox/browser/defaults/preferences/firefox-redhat-default-prefs.js
 
 # Flatpaks
 flatpak install -y flathub com.transmissionbt.Transmission org.libreoffice.LibreOffice com.github.tchx84.Flatseal com.github.finefindus.eyedropper org.kde.krita \
@@ -58,7 +82,7 @@ flatpak install -y flathub com.transmissionbt.Transmission org.libreoffice.Libre
 	org.prismlauncher.PrismLauncher com.discordapp.Discord org.freedesktop.Platform.ffmpeg-full net.lutris.Lutris org.localsend.localsend_app
 
 # Additional Packages
-sudo dnf install -y ansible git steam-devices neovim sqlite3 zsh zsh-autosuggestions zsh-syntax-highlighting setroubleshoot ffmpeg compat-ffmpeg4 akmod-v4l2loopback yt-dlp \
+sudo dnf install -y ansible git steam-devices neovim sqlite3 zsh zsh-autosuggestions zsh-syntax-highlighting setroubleshoot ffmpeg compat-ffmpeg4 ffmpeg-libs libva libva-utils akmod-v4l2loopback yt-dlp \
 	@virtualization guestfs-tools distrobox podman kdevelop plasma-nm kontact korganizer gamemode kate vlc vlc-plugin-pipewire --best --allowerasing
 
 # Even more Packages
@@ -80,7 +104,9 @@ sudo dnf install mullvad-vpn
 sudo dnf install python3-pyqt6 libsecp256k1 python3-cryptography python3-setuptools python3-pip
 
 # Multi Media
+sudo dnf4 group install multimedia
 sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf group install -y sound-and-video # Installs useful Sound and Video complementary packages.
 
 # Set Hostname
 sudo hostnamectl set-hostname **new-hostname**
